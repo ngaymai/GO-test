eb5620262aa1b9ee23e56a45180e6b68c248b5ed
ntr18 <nghia.nguyen.tr18@gmail.com>
2024-01-04 00:34:12 +0700

    third commit

--- inc/config.php
+++ inc/config.php
@@ -1,11 +1,10 @@
 <?php
 define("PROJECT_ROOT_PATH", __DIR__ . "/../");
 // include main configuration file 
-require_once PROJECT_ROOT_PATH . "/inc/config.php";
+// require_once PROJECT_ROOT_PATH . "/inc/config.php";
 // include the base controller file 
-require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
-require_once PROJECT_ROOT_PATH . "/Controller/Api/ProductController.php";
-require_once PROJECT_ROOT_PATH . "/Controller/Api/RoomController.php";
+require_once PROJECT_ROOT_PATH . "/Controller/BaseController.php";
+require_once PROJECT_ROOT_PATH . "/Controller/ProductController.php";
+
 // include the use model file 
 require_once PROJECT_ROOT_PATH . "/Model/ProductModel.php";
-require_once PROJECT_ROOT_PATH . "/Model/RoomModel.php";
